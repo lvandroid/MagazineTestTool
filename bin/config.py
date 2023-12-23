5340311d6f938373dfacd791f98591531a90a0dc
lvandroid <lvandroid@outlook.com>
2023-12-24 01:23:06 +0800

    新增配置解码

--- bin/config.py
+++ bin/config.py
@@ -4,23 +4,45 @@ from cmd import (
 )
 from flet import (
     ElevatedButton,
+    UserControl,
+    Row,
+    Column,
+    TextField,
 )
+
 
 def load_json_file(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
 
+
 # 获取当前脚本所在的目录
 curr_dir = os.path.dirname(os.path.realpath(__file__))
 # 构建JSON文件的路径
-setting_json_file_path = os.path.join(curr_dir, "..", "conf", "settings.json")
+config_path = os.path.join(curr_dir, "..", "conf", "config.json")
+config = load_json_file(config_path)
+
+
+class CmdPanel(UserControl):
+    def build(self):
+        self.terminal = TextField(width=200)
+        return Row([
+            self.terminal,
+            Column([], expand=True)
+        ], expand=True)
+
+
+# 根据配置文件加载对应的脚本命令
+def load_scripts(conf_path):
+    full_path = os.path.join(curr_dir, "..", "conf", conf_path)
+    scripts_config = load_json_file(full_path)
 
 
 def loading_settings(log_view, cmd_panel):
-    if not os.path.exists(setting_json_file_path):
+    if not os.path.exists(config_path):
         logging.error("settings.json not exists")
         return {}
-    with open(setting_json_file_path, "r", encoding="utf-8") as file:
+    with open(config_path, "r", encoding="utf-8") as file:
         config = json.load(file)
         logging.debug(config)
         for item in config["adb_cmds"]:
